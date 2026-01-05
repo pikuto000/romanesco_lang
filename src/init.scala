6d6aba9d37fb3e852e4aa2095f25d52443437fed
pikuto <cocoatopotopo@hotmail.com>
2026-01-05 10:43:33 +0900

    文体の細かい変更

--- src/init.scala
+++ src/init.scala
@@ -2,33 +2,32 @@ package romanesco
 import scala.util.parsing.combinator._
 
 object init {
-
+  
   object lex{
     def setup(lexName:String)={
       val lexer=new Lexer(Hygenicmarker.bless(s"lexer:${lexName}",None,true))
       core(lexer)
       lexer
     }
-
+    
     def core(lexer:Lexer):Unit={
       lexer.database.set(
-        Hygenicmarker.bless("whiteSpace",Some(lexer),true),
-        lexer.regex("""\s+""".r) ^^ {
-          ws => lexer.Token.otherwise(ws,Hygenicmarker.bless(s"whiteSpace:'${ws}'",Some(lexer),true))
-        }
+      Hygenicmarker.bless("whiteSpace",Some(lexer),true),
+      lexer.regex("""\s+""".r) ^^ {
+        ws => lexer.Token.otherwise(ws,Hygenicmarker.bless(s"whiteSpace:'${ws}'",Some(lexer),true))
+      }
       )
       lexer.database.set(
-        Hygenicmarker.bless("word",Some(lexer),true),
-        lexer.regex("""\S+""".r) ^^ {
-          word => lexer.Token.otherwise(word,Hygenicmarker.bless(s"word:${word}",Some(lexer),true))
-        }
+      Hygenicmarker.bless("word",Some(lexer),true),
+      lexer.regex("""\S+""".r) ^^ {
+        word => lexer.Token.otherwise(word,Hygenicmarker.bless(s"word:${word}",Some(lexer),true))
+      }
       )
     }
-
+    
   }
-
+  
   object parse{
     ???
   }
-
 }
