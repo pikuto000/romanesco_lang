c33be45463f8952d0ebbb50869c2c6bd2e067f6b
pikuto <cocoatopotopo@hotmail.com>
2026-02-05 20:28:05 +0900

    texts.ps1 を更新

--- texts.ps1
+++ texts.ps1
@@ -1,1 +1,15 @@
-#create text
+$sourceFolder   = "C:\input"
+$exportFolder   = "C:\output\抽出結果"
+
+# 出力フォルダーがなければ作る
+if (-not (Test-Path $exportFolder)) { New-Item -Path $exportFolder -ItemType Directory }
+
+Get-ChildItem -Path $sourceFolder -File -Recurse -Filter "*.txt" |
+    ForEach-Object {
+        $destName = $_.FullName -replace [regex]::Escape($sourceFolder), ""
+        $destName = $destName -replace '[\\/]', '_'
+        $destPath = Join-Path $exportFolder $destName
+
+        "===== $($_.Name) =====" | Out-File -FilePath $destPath -Encoding utf8
+        Get-Content $_.FullName -Encoding UTF8 -Raw | Out-File -FilePath $destPath -Encoding utf8 -Append
+    }
